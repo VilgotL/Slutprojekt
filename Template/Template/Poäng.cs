e9b124c57224131bdffadf39fe8bfd86f41930c4
VilgotL <54435285+VilgotL@users.noreply.github.com>
2020-04-28 13:54:10 +0200

    Add files via upload

---
+++ Template/Template/Poäng.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Audio;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using System;
+
+namespace Template
+{
+	class Poäng
+	{
+		private Vector2 position;
+		SpriteFont text;
+		int poäng = 0;
+
+		public Poäng(Vector2 position, SpriteFont text)
+		{
+			this.position = position;
+			this.text = text;
+		}
+
+		public virtual void DrawString(SpriteBatch spriteBatch)
+		{
+			spriteBatch.DrawString(text, poäng.ToString(), position, Color.White);
+		}
+
+		public virtual void AddPoint()
+		{
+			poäng++;
+		}
+	}
+}
